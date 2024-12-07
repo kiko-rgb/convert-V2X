c4e94097179f32973a6d0842e02ee8caa416f439
IkonomouKonstantin <konstantin.ikonomou@tum.de>
2024-12-08 00:41:05 +0100

    first working prototype for all 5 components

--- v2x2roscenes.py
+++ v2x2roscenes.py
@@ -10,12 +10,12 @@ def values_exist(data, key, values, categories = False):
         missing_values = [value for value in values if value not in existing_values]
         return len(missing_values) == 0, missing_values
 
-def transform_to_matrix(transform):
+def transform_to_matrix(transform, transposed = False):
     R = np.array(transform['rotation']).reshape(3, 3)
     t = np.array(transform['translation']).reshape(3,)
     matrix = np.eye(4)
-    matrix[:3, :3] = R
-    matrix[:3, 3] = t
+    matrix[:3, :3] = R if not transposed else R.T
+    matrix[:3, 3] = t if not transposed else - R.T @ t
     return matrix
     
 def convert_v2x_to_new_format(v2x_path: str):
@@ -47,7 +47,7 @@ def convert_v2x_to_new_format(v2x_path: str):
 
         veh_id = sync_id * 2
         inf_id = veh_id + 1
-        # --- IMAGES ---
+        # ========================= IMAGES =========================
         # add the vehicle image
         vehicle_dict = {
             'id': veh_id,
@@ -70,7 +70,7 @@ def convert_v2x_to_new_format(v2x_path: str):
         }
         data['images'].append(infrastructure_dict)
 
-        # --- FRAMES ---
+        # ========================= FRAMES =========================
         # add the vehicle image
         frame_vehicle_dict = {
             'id': veh_id, 
@@ -93,7 +93,7 @@ def convert_v2x_to_new_format(v2x_path: str):
 
         data['frames'].append(frame_infrastructure_dict)
     
-        # --- CAMERAS ---
+        # ========================= CAMERAS =========================
         # vehicle side
         vehicle_intrinsics = json.load(open(v2x_vehicle_path + vehicle_data[sync_id]['calib_camera_intrinsic_path'], 'r'))
         lidar_to_camera = json.load(open(v2x_vehicle_path + vehicle_data[sync_id]['calib_lidar_to_camera_path'], 'r'))
@@ -101,20 +101,19 @@ def convert_v2x_to_new_format(v2x_path: str):
         novatel_to_world = json.load(open(v2x_vehicle_path + vehicle_data[sync_id]['calib_novatel_to_world_path'], 'r'))
         
         # TODO: camera distortion parameters 'cam_D' currently not considered
-        R_lidar_to_camera = transform_to_matrix(lidar_to_camera)
+        R_lidar_to_camera = transform_to_matrix(lidar_to_camera, transposed = True) # needs to be .T, as it is given from lidar to camera
         R_lidar_to_novatel = transform_to_matrix(lidar_to_novatel['transform'])
         R_novatel_to_world = transform_to_matrix(novatel_to_world)
 
-        extrinsics_vehicle = R_novatel_to_world @ R_lidar_to_novatel @ R_lidar_to_camera.T # NOTE: the last row is not equal to 0, 0, 0, 1 # lidar to camera, needs .T
-
+        extrinsics_vehicle = R_novatel_to_world @ R_lidar_to_novatel @ R_lidar_to_camera 
         camera_vehicle_dict = {
             'extrinsics': extrinsics_vehicle.tolist(),
             'intrinsics': [vehicle_intrinsics['cam_K'][i:i+3] for i in range(0, len(vehicle_intrinsics['cam_K']), 3)],
             'id': veh_id, # NOTE: this should be handled differently for the infrastructure, as the camera is stationary !!
-            'max_x': int, # TODO: maybe both camera dicts need an additional parameter to flag if camera is from a vehicle or infrastructure?
-            'max_y': int,
-            'min_x': int,
-            'min_y': int,
+            'max_x': 1000, # TODO: maybe both camera dicts need an additional parameter to flag if camera is from a vehicle or infrastructure?
+            'max_y': 1000,
+            'min_x': -1000,
+            'min_y': -1000, # TODO: these are placeholder values!!!
         }
 
         data['cameras'].append(camera_vehicle_dict)
@@ -131,10 +130,10 @@ def convert_v2x_to_new_format(v2x_path: str):
             virtual_lidar_to_camera = json.load(open(v2x_infrastructure_path + infrastructure_data[sync_id]['calib_virtuallidar_to_camera_path'], 'r'))
             virtual_lidar_to_world = json.load(open(v2x_infrastructure_path + infrastructure_data[sync_id]['calib_virtuallidar_to_world_path'], 'r'))
 
-            R_virtual_lidar_to_camera = transform_to_matrix(virtual_lidar_to_camera)
+            R_virtual_lidar_to_camera = transform_to_matrix(virtual_lidar_to_camera, transposed = True) # needs to be .T, as it is given from lidar to camera
             R_virtual_lidar_to_world = transform_to_matrix(virtual_lidar_to_world)
 
-            extrinsics_infrastructure = R_virtual_lidar_to_world @ R_virtual_lidar_to_camera.T # NOTE: the last row is not equal to 0, 0, 0, 1
+            extrinsics_infrastructure = R_virtual_lidar_to_world @ R_virtual_lidar_to_camera 
 
             camera_infrastructure_dict = {
                 'extrinsics': extrinsics_infrastructure.tolist(),
@@ -148,7 +147,7 @@ def convert_v2x_to_new_format(v2x_path: str):
 
             data['cameras'].append(camera_infrastructure_dict)
 
-        # --- ANNOTATIONS ---
+        # ========================= ANNOTATIONS =========================
         # NOTE: - Truncated: Integer [0, 1, 2] indicating 3 types of truncated state, non-truncated, transversely truncated, longitudinally truncated respectively.
         # ----- Truncated is not clear to me.
 
@@ -160,37 +159,69 @@ def convert_v2x_to_new_format(v2x_path: str):
         for ann in vehicle_annotations:
             # check if any object type was not encountered before
             values_to_check = ann['type'].lower()
-            exists, missing_categorie = values_exist(data = data['categories'], key = 'name', values = values_to_check, categories = False) # Refactor function, both use categories=False
+            exists, missing_categorie = values_exist(data = data['categories'], key = 'name', values = values_to_check, categories = False) # TODO: Refactor function, both use categories=False
+            # add it to categories if it did not exist there before
             if not exists:
                 data['categories'].append({'id': len(data['categories']) + 1, 'name': missing_categorie})
 
-            # get object (= ann) position in world frame
-
+            # get object (= ann) position in world frame; objects are noted in LiDAR/virtual LiDAR frame, but should be in world frame:
+            location_in_world_frame = R_novatel_to_world @ R_lidar_to_novatel @ np.vstack((np.array(list(ann['3d_location'].values())).reshape(-1, 1), [[1]]))
             # get object rotation matrix/rotvec
-            # TODO: continue here
-            # egoc_rot_matrix = Rotation.from_euler('xyz', [np.pi / 2, ann['rotation_y'], 0]).as_matrix()
-            egoc_rot_matrix = Rotation.from_euler('xyz', [np.pi / 2, ann['rotation_y'], 0]).as_rotvec()
+            egoc_rot_matrix = Rotation.from_euler('xyz', [np.pi / 2, ann['rotation'], 0]).as_rotvec() # TODO: if this gives trouble, try other values instead of np.pi/2
             
             # get occlusion score: from V2X [0, 1, 2] (fully visible, partly occluded, largely occluded) to RoScenes [float]
             occlusion_score = 0 if ann['occluded_state'] == 0 else 0.5 if ann['occluded_state'] == 1 else 1
 
-
             vehicle_annotation_dict = {
                 'id': ann_id,
                 'frame_id': sync_id, # NOTE: !!! This is experimental, meaning all veh and inf are grouped together with another -> differs from previous approach(?)
-                'location': [0.0, 0.0, 0.0],  
-                'dim': [ann['3d_location']['x'], ann['3d_location']['y'], ann['3d_location']['z']], 
-                'rotvec': [0.0, 0.0, 0.0],  
+                'location': location_in_world_frame.reshape(4, ).tolist()[:-1],  
+                'dim': [ann['3d_dimensions']['l'], ann['3d_dimensions']['w'], ann['3d_dimensions']['h']], 
+                'rotvec': egoc_rot_matrix.tolist(), # TODO: questionable if this is already good to go, w.r.t correct coordinate system 
                 'category_id': next((d['id'] for d in data['categories'] for key, value in d.items() if value == ann['type'].lower()), None),  
                 'track_id': int(ann['track_id']), 
                 'occlusion': occlusion_score, 
                 'velocity': [0.0, 0.0]  # NOTE: 2D velocity will be not filled out?
             }
 
-            ann_id += 1
+            ann_id += 1 # NOTE: do not touch this line!!!
             data['annotations'].append(vehicle_annotation_dict)
+
+        # infrastructure side
+        for ann in infrastructure_annotations:
+            # check if any object type was not encountered before
+            values_to_check = ann['type'].lower()
+            exists, missing_categorie = values_exist(data = data['categories'], key = 'name', values = values_to_check, categories = False) # TODO: Refactor function, both use categories=False
+            # add it to categories if it did not exist there before
+            if not exists:
+                data['categories'].append({'id': len(data['categories']) + 1, 'name': missing_categorie})
+
+            # get object (= ann) position in world frame; objects are noted in LiDAR/virtual LiDAR frame, but should be in world frame:
+            location_in_world_frame = R_virtual_lidar_to_world @ np.vstack((np.array(list(ann['3d_location'].values())).reshape(-1, 1), [[1]]))
+            # get object rotation matrix/rotvec
+            egoc_rot_matrix = Rotation.from_euler('xyz', [np.pi / 2, ann['rotation'], 0]).as_rotvec() # TODO: if this gives trouble, try other values instead of np.pi/2
+            
+            # get occlusion score: from V2X [0, 1, 2] (fully visible, partly occluded, largely occluded) to RoScenes [float]
+            occlusion_score = 0 if ann['occluded_state'] == 0 else 0.5 if ann['occluded_state'] == 1 else 1
+
+            infrastructure_annotation_dict = {
+                'id': ann_id,
+                'frame_id': sync_id, # NOTE: !!! This is experimental, meaning all veh and inf are grouped together with another -> differs from previous approach(?)
+                'location': location_in_world_frame.reshape(4, ).tolist()[:-1],  
+                'dim': [ann['3d_dimensions']['l'], ann['3d_dimensions']['w'], ann['3d_dimensions']['h']], 
+                'rotvec': egoc_rot_matrix.tolist(), # TODO: questionable if this is already good to go, w.r.t correct coordinate system 
+                'category_id': next((d['id'] for d in data['categories'] for key, value in d.items() if value == ann['type'].lower()), None),  
+                'track_id': int(ann['track_id']), 
+                'occlusion': occlusion_score, 
+                'velocity': [0.0, 0.0]  # NOTE: 2D velocity will be not filled out?
+            }
+
+            ann_id += 1 # NOTE: do not touch this line!!!
+            data['annotations'].append(infrastructure_annotation_dict)
+
         print(sync_id)
-        # --- CATEGORIES ---
+    return data
+        # ========================= CATEGORIES =========================
         # first consider all objects visible from the vehicle:
         # maybe put this into annotations loop
         """values_to_check = set([i['type'].lower() for i in camera_annotations])
@@ -199,15 +230,15 @@ def convert_v2x_to_new_format(v2x_path: str):
             for cat in missing_categorie:
                 data['categories'].append({'id': len(data['categories']) + 1, 'name': cat})"""
 
-    return data
+
 
 
 def main():
     v2x_path = '/home/stud/iko/storage/group/deepscenario/v2x-seq/v2x-seq/V2X-Seq-SPD'
     save_path = '/home/stud/iko/test/V2X-Seq-test/v2x.json'
     new_data = convert_v2x_to_new_format(v2x_path)
-
-    json.dump(new_data, open(save_path, 'w'))
+    with open(save_path, 'w') as file:
+        json.dump(new_data, file)
 
 
 
